c9d637df868a482770d4210cde80a0ec0d305635
Purva-Jani <66566447+Purva-Jani@users.noreply.github.com>
2023-07-23 23:35:29 -0400

    added files

---
+++ 2048.js
@@ -0,0 +1,23 @@
+const board;
+let score = 0;
+const rows = 4;
+const columns = 4;
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    board = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+    ]
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+        }
+    }
+}
